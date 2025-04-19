1fdfaca2469d7ca13b22d923e68ab51772d0b972
vitabin <121412096+vitabin@users.noreply.github.com>
2025-04-19 15:32:47 +0900

    불필요한 필드 삭제

--- src/main/java/com/catchtable/api/admin/DTO/UploadCouponDTO.java
+++ src/main/java/com/catchtable/api/admin/DTO/UploadCouponDTO.java
@@ -1,7 +1,6 @@
 package com.catchtable.api.admin.DTO;
 
 import com.catchtable.api.file.domain.FileType;
-import jakarta.validation.constraints.Null;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,31 +11,7 @@ public class UploadCouponDTO {
 
     private final MultipartFile file;
 
-    @Null
-    private FileType fileType;
-
-    @Null
-    private String filename;
-
-    public UploadCouponParam toPrams(String token) {
+    public UploadCouponParam toPrams(String token, String filename, FileType fileType) {
         return new UploadCouponParam(token, file, filename, fileType);
-    }
-
-    public void validate() {
-        filename = file.getOriginalFilename();
-
-//      TODO: custom exception으로 변경
-        if (filename == null) {
-            throw new RuntimeException("Filename can be null");
-        }
-
-        String extension = filename.substring(filename.lastIndexOf('.'))
-                                   .toLowerCase();
-        fileType = FileType.getFileType(extension);
-
-//      TODO: custom exception으로 변경
-        if (fileType != FileType.CSV && fileType != FileType.XLS && fileType != FileType.XLSX) {
-            throw new RuntimeException("Unsupported file extension: " + extension);
-        }
     }
 }
--- src/main/java/com/catchtable/api/admin/controller/AdminController.java
+++ src/main/java/com/catchtable/api/admin/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.catchtable.api.admin.controller;
 
 import com.catchtable.api.admin.DTO.UploadCouponDTO;
 import com.catchtable.api.admin.service.AdminService;
+import com.catchtable.api.file.domain.FileType;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
@@ -11,11 +12,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/admin")
@@ -27,10 +28,24 @@ public class AdminController {
     // TODO: response 변경
     @PostMapping("/coupons")
     public void uploadCoupons(@RequestHeader("Authorization") String token,
-        @RequestParam("file") MultipartFile file) {
-        UploadCouponDTO uploadCouponDTO = new UploadCouponDTO(file);
-        uploadCouponDTO.validate();
-        adminService.uploadCoupon(uploadCouponDTO.toPrams(token));
+        @RequestBody UploadCouponDTO uploadCouponDTO) {
+        String filename = uploadCouponDTO.getFile()
+                                         .getOriginalFilename();
+
+//      TODO: custom exception으로 변경
+        if (filename == null) {
+            throw new RuntimeException("Filename can be null");
+        }
+
+        String extension = filename.substring(filename.lastIndexOf('.'))
+                                   .toLowerCase();
+        FileType fileType = FileType.getFileType(extension);
+
+//      TODO: custom exception으로 변경
+        if (!FileType.isCouponExtension(extension)) {
+            throw new RuntimeException("Unsupported file extension: " + extension);
+        }
+        adminService.uploadCoupon(uploadCouponDTO.toPrams(token, filename, fileType));
     }
 
     @DeleteMapping("/coupons/{id}")
